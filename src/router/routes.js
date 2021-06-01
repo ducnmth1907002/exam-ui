f461c366402bb8f746f58426055c3969ecdea2b5
Your Name <ducnmth1907002@fpt.edu.vn>
2021-06-01 20:27:43 +0700

    remove unused view

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,10 @@
 
 const routes = [
-  // { path: '/', component: () => import('pages/quotes.vue') },
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      // { path: '/dashboard', component: () => import('pages/dashboard.vue') },
-      // { path: '/dashboard_v2', component: () => import('pages/dashboard_v2.vue') },
-      // { path: '/dashboard_v3', component: () => import('pages/dashboard_v3.vue') },
       { path: '/', component: () => import('pages/customer_management.vue') },
-      // { path: '/change_request', component: () => import('pages/change_request.vue') },
-      // { path: '/my_profile', component: () => import('pages/my_profile.vue') },
-      // { path: '/sales_invoices', component: () => import('pages/sales_invoices.vue') },
-      // { path: '/quotes', component: () => import('pages/quotes.vue') },
-      // { path: '/transactions', component: () => import('pages/transactions.vue') },
-      // { path: '/employee_salary_list', component: () => import('pages/employee_salary_list.vue') },
-      // { path: '/calendar', component: () => import('pages/calendar.vue') },
-      // { path: '/department', component: () => import('pages/department.vue') },
     ]
   }
 ]
